0acf82767e7481c2199aba47d5b5a66e2513331b
hamin07 <haminyang2@naver.com>
2025-04-29 20:27:26 +0900

    .

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,5 +1,6 @@
 import '../../assets/style/pages/home/HomePage.css';
 import Home from '../../components/home/Home';
+import Home from '../../components/home/Home';
 import React from 'react';
 import { Link } from 'react-router-dom'
 
